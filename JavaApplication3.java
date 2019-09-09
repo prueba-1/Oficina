5336053a75c8ba042fc1b31c09d6bef84532899d
Diegote36 <54809566+Diegote36@users.noreply.github.com>
2019-09-09 10:02:37 -0500

    Add files via upload

---
+++ JavaApplication3.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+class Oficina{
+
+     String n_oficina;
+     public Oficina(){         
+        this.n_oficina="";
+     }
+
+    public String getN_oficina() {
+        return n_oficina;
+    }
+
+    public void setN_oficina(String n_oficina) {
+        this.n_oficina = n_oficina;
+    }
+
+    @Override
+    public String toString() {
+        return "Oficina{" + "n_oficina=" + n_oficina + '}';
+    }
+
+   
+    
+}
+public class JavaApplication3 {
+
+  
+    public static void main(String[] args) {
+    
+    }
+    
+}
